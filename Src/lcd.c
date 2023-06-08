691b7c8132b9faea2a468a38f1f7b78ff19d639f
liou0006 <135958682+liou0006@users.noreply.github.com>
2023-06-08 16:16:58 +0200

    Add files via upload

---
+++ Src/lcd.c
@@ -0,0 +1,17 @@
+/*
+ * LCDfunc.c
+ *
+ *  Created on: 8 Jun 2023
+ *      Author: liou-
+ */
+
+#include "charset.h"
+#include "LCDfunc.h"
+
+void lcd_write_string(char s[], int x, int y){
+
+}
+
+void lcd_update(){
+
+}
