ed743e9fa995ceaf1f97fb542984fbd96352e46a
liou0006 <135958682+liou0006@users.noreply.github.com>
2023-06-09 15:05:37 +0200

    Wrote the code for lcd_write_text()

--- Src/lcd.c
+++ Src/lcd.c
@@ -6,12 +6,28 @@
  */
 
 #include "charset.h"
-#include "LCDfunc.h"
+#include "lcd.h"
+#include "30010_io.h"
+#include "stm32f30x_conf.h" // STM32 config
+#include "stdio.h"
+#include "stdint.h"
+#include "string.h"
 
-void lcd_write_string(char s[], int x, int y){
-
+void lcd_write_text(char TtD[], int line){
+	lcd_init();
+	uint8_t buffer[512];
+	memset(buffer,0x00,512);
+	lcd_push_buffer(buffer);
+	int offset = line;
+	for(int i = 0; i < strlen(TtD); i++){
+		for(int j = 0; j < 5;j++){
+			//character_data[[32;126]
+			buffer[j+(5*i)+(128*offset)] = character_data[TtD[i]-32][j];
+		}
+	}
+	lcd_push_buffer(buffer);
 }
 
-void lcd_update(){
-
+void lcd_update(char TtD[], int line, int x){
+	
 }
