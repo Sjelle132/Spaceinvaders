289687ddb9fe7be59579b4880d429da0b575b446
liou0006 <135958682+liou0006@users.noreply.github.com>
2023-06-13 13:46:54 +0200

    added tid.flag for lcd.c

--- Src/timers.c
+++ Src/timers.c
@@ -6,9 +6,8 @@
  */
 
 #include "timers.h"
-#include "pins.h"
 
-timeS_t tid = {0,0,0,0};
+timeS_t tid = {0,0,0,0,0};
 
 void configTimer(){
 	RCC->APB2ENR |= RCC_APB2Periph_TIM15; // Enable clock line to timer 15;
@@ -19,15 +18,15 @@ void configTimer(){
 	TIM15->CR1 = 1; // Configure timer 15
 
 	NVIC_SetPriority(TIM1_BRK_TIM15_IRQn, 0); // Set interrupt priority
-	NVIC_EnableIRQ(TIM1_BRK_TIM15_IRQn); // Disable interrupt
+	NVIC_EnableIRQ(TIM1_BRK_TIM15_IRQn); // Enable interrupt
 }
 
 void TIM1_BRK_TIM15_IRQHandler(void) {
-
 	tid.miliseconds++;
 	if (tid.miliseconds == 1000){
 		tid.seconds++;
 		tid.miliseconds = 0;
+		//tid.flag = 1;
 	}else if (tid.seconds == 60){
 		tid.minutes++;
 		tid.seconds = 0;
@@ -40,26 +39,20 @@ void TIM1_BRK_TIM15_IRQHandler(void) {
 		tid.minutes = tid.minutes;
 		tid.hours = tid.hours;
 	}
+
+	//set flag high for lcdTimeDisplay under lcd.c
+	tid.flag = 1;
+
 	TIM15->SR &= ~0x0001; // Clear interrupt bit
-
-
 }
 
 int enableSomething = 0;
 
-int32_t returnSec(){
-		return tid.seconds;
-}
-
-int32_t returnMilisec(){
-	return tid.miliseconds;
-}
-
 void prntClk(){
-
 	int32_t joystickState = readJoystick();
 	gotoxy(1,4);
 	printf("%d",enableSomething);
+
 	if (joystickState == 16){
 		if(enableSomething == 1){
 			NVIC_DisableIRQ(TIM1_BRK_TIM15_IRQn);
@@ -73,11 +66,9 @@ void prntClk(){
 		gotoxy(1,2);
 		printf("%02d:%02d:%02d:%03d",tid.hours,tid.minutes,tid.seconds,tid.miliseconds);
 
-
 	}else if (joystickState == 8){
 		gotoxy(1,3);
 		printf("%02d:%02d:%02d:%03d",tid.hours,tid.minutes,tid.seconds,tid.miliseconds);
-
 
 	}else if (joystickState == 2){
 		NVIC_DisableIRQ(TIM1_BRK_TIM15_IRQn);
@@ -92,6 +83,7 @@ void prntClk(){
 		printf("%02d:%02d:%02d:%03d",tid.hours,tid.minutes,tid.seconds,tid.miliseconds);
 		gotoxy(1,3);
 		printf("%02d:%02d:%02d:%03d",tid.hours,tid.minutes,tid.seconds,tid.miliseconds);
+
 	}else {
 		gotoxy(1,1);
 		printf("%02d:%02d:%02d",tid.hours,tid.minutes,tid.seconds);
@@ -99,6 +91,10 @@ void prntClk(){
 	}
 }
 
+int32_t returnSec(){
+	return tid.seconds;
+}
 
-
-
+int32_t returnMilisec(){
+	return tid.miliseconds;
+}
