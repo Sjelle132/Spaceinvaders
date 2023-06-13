ec8619ab0217056494e06b987e928a75f1efb5b8
Sjelle132 <117978670+Sjelle132@users.noreply.github.com>
2023-06-13 16:11:16 +0200

    Add files via upload

--- Src/spaceship.c
+++ Src/spaceship.c
@@ -15,8 +15,8 @@
 //initialize spaceship
 
 void initSpaceship(spaceship_t* spaceship) {
-	spaceship->posX = 0;
-	spaceship->posY = 0;
+	spaceship->posX = 5;
+	spaceship->posY = 20;
 	spaceship->velX = 0;
 	spaceship->velY = 0;
 	spaceship->life = 3;
@@ -24,28 +24,115 @@ void initSpaceship(spaceship_t* spaceship) {
 
 //create spaceship
 void createSpaceship(spaceship_t* spaceship) {
-	if (spaceship->life > 0){
+
 		gotoxy(spaceship->posX,spaceship->posY);
 		printf("%c", 219);
+		gotoxy(spaceship->posX ,spaceship->posY + 1);
+		printf("%c", 219);
+		gotoxy(spaceship->posX ,spaceship->posY + 2);
+		printf("%c", 219);
+		gotoxy(spaceship->posX ,spaceship->posY - 1);
+		printf("%c", 219);
+		gotoxy(spaceship->posX ,spaceship->posY - 2 );
+		printf("%c", 219);
 
-	}
+		gotoxy(spaceship->posX +1 ,spaceship->posY - 1 );
+		printf("%c", 219);
+		gotoxy(spaceship->posX +1 ,spaceship->posY  );
+		printf("%c", 219);
+		gotoxy(spaceship->posX +1 ,spaceship->posY + 1 );
+		printf("%c", 219);
+
+		gotoxy(spaceship->posX +2 ,spaceship->posY);
+		printf("%c", 219);
+
+
+
+		//printf("%c", 219);
+
+
 }
 
 //update spaceship position
-void updateSpaceship(spaceship_t* spaceship){
+void updateSpaceship(spaceship_t* spaceship, uint8_t joystickState){
 	spaceship->posX = spaceship->posX + spaceship->velX;
 	spaceship->posY = spaceship->posY + spaceship->velY;
 
-	//uint8_t joystickState = readJoystick();
-	uint8_t joystickState = keyboardController();
-
 	//pin down
-	if (joystickState == 2){
+	if (spaceship->life > 0 ) {
+		if (joystickState == 2 ){
+
+			spaceship->posY += 1;
+			spaceship->velY = 1;
+			spaceship->velX = 0;
+
+		}else if (joystickState == 1 ) {
+			//pin up
+			spaceship->posY -= 1;
+			spaceship->velY = -1;
+			spaceship->velX = 0;
+
+		}else if (joystickState == 4 ){
+			//pin left
+
+			spaceship->posX -= 1;
+			spaceship->velY = 0;
+			spaceship->velX = -1;
+		}else if (joystickState == 8 ){
+			//pin right
+
+			spaceship->posX += 1;
+			spaceship->velY = 0;
+			spaceship->velX = 1;
+		}
+	}
+}
+
+//delete the prev spaceship
+void removeSpaceship(spaceship_t* spaceship){
+
+	//uint8_t joystickState = keyboardController();
+
+
+			gotoxy(spaceship->posX,spaceship->posY);
+			printf("%c", 32);
+			gotoxy(spaceship->posX ,spaceship->posY + 1);
+			printf("%c", 32);
+			gotoxy(spaceship->posX ,spaceship->posY + 2);
+			printf("%c", 32);
+			gotoxy(spaceship->posX ,spaceship->posY - 1);
+			printf("%c", 32);
+			gotoxy(spaceship->posX ,spaceship->posY - 2 );
+			printf("%c", 32);
+
+			gotoxy(spaceship->posX +1 ,spaceship->posY - 1 );
+			printf("%c", 32);
+			gotoxy(spaceship->posX +1 ,spaceship->posY  );
+			printf("%c", 32);
+			gotoxy(spaceship->posX +1 ,spaceship->posY + 1 );
+			printf("%c", 32);
+
+			gotoxy(spaceship->posX +2 ,spaceship->posY);
+			printf("%c", 32);
+
+
+	//gotoxy(spaceship->posX,spaceship->posY);
+	//printf("%c", 32);
+}
+
+
+
+
+
+/*
+
+switch(joystickState){
+	case 2:
 		spaceship->posY += 1;
 		spaceship->velY = 1;
 		spaceship->velX = 0;
 		//rotateVector((&spaceship->posX,&spaceship->posY), 5);
-
+		break;
 		//pin up
 	}else if (joystickState == 1){
 		spaceship->posY -= 1;
@@ -79,18 +166,5 @@ void updateSpaceship(spaceship_t* spaceship){
 	}
 
 
-}
-
-//delete the prev spaceship
-void removeSpaceship(spaceship_t* spaceship){
-	gotoxy(spaceship->posX,spaceship->posY);
-	printf("%c", 32);
-}
-
-
-
-
-
-
-
+}*/
 
