e7d1f017c858f30cb6e1819a50936c4f64426d4e
Sjelle132 <117978670+Sjelle132@users.noreply.github.com>
2023-06-14 14:17:08 +0200

    Updated enemies Header file

---
+++ Inc/enemies.h
@@ -0,0 +1,40 @@
+/*
+ * enemies.h
+ *
+ *  Created on: 13. jun. 2023
+ *      Author: oscar
+ */
+
+#ifndef ENEMIES_H_
+#define ENEMIES_H_
+
+#include "stm32f30x_conf.h"
+
+typedef struct {
+
+	int16_t posX;
+	int16_t posY;
+	int16_t velX;
+	int16_t velY;
+	int8_t life;
+
+
+} enemies_t;
+
+typedef struct {
+
+	int16_t posX;
+	int16_t posY;
+	int16_t velX;
+	int16_t velY;
+	int8_t life;
+
+} eliteEnemy_t;
+
+void initEliteEnemy(eliteEnemy_t* EliteEnemy);
+void initEnemies(enemies_t enemies[]);
+void createEnemies(enemies_t enemies[],int8_t* f);
+void updateEnemies(enemies_t enemies[],int8_t* f, int16_t flag);
+void removeEnemies(enemies_t enemies[],int8_t* f, int16_t flag);
+
+#endif /* ENEMIES_H_ */
