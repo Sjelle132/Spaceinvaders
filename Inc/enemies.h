274fb83b233ccbc142ccd122556a9cac60f72237
Sjelle132 <117978670+Sjelle132@users.noreply.github.com>
2023-06-19 11:55:15 +0200

    Updated a H filer til Liou

--- Inc/enemies.h
+++ Inc/enemies.h
@@ -9,6 +9,9 @@
 #define ENEMIES_H_
 
 #include "stm32f30x_conf.h"
+#include "spaceship.h"
+
+
 
 typedef struct {
 
@@ -16,25 +19,34 @@ typedef struct {
 	int16_t posY;
 	int16_t velX;
 	int16_t velY;
-	int8_t life;
+	uint8_t life;
 
 
 } enemies_t;
 
+
+
 typedef struct {
 
 	int16_t posX;
 	int16_t posY;
-	int16_t velX;
-	int16_t velY;
-	int8_t life;
+	int8_t true;
 
-} eliteEnemy_t;
+} enemyBullet_t;
 
-void initEliteEnemy(eliteEnemy_t* EliteEnemy);
+
 void initEnemies(enemies_t enemies[]);
-void createEnemies(enemies_t enemies[],int8_t* f);
-void updateEnemies(enemies_t enemies[],int8_t* f, int16_t flag);
-void removeEnemies(enemies_t enemies[],int8_t* f, int16_t flag);
+void createEnemies(enemies_t enemies[]);
+void updateEnemies(enemies_t enemies[] );
+void removeEnemies(enemies_t enemies[]);
+
+void initEnemyBullet(enemies_t enemies[],enemyBullet_t enemyBullet[]) ;
+void enemyShoot(enemyBullet_t enemyBullet[],enemies_t enemies[]);
+void updateEnemyShoot(enemyBullet_t enemyBullet[], enemies_t[]);
+void removeEnemyShoot(enemyBullet_t enemyBullet[]);
+
+void interactionsEnemyBulletHitPlayer(enemyBullet_t enemyBullet[], spaceship_t* spaceship);
+void isAllEnemyDead(enemies_t enemies[], int8_t f,spaceship_t spaceship);
+
 
 #endif /* ENEMIES_H_ */
--- Inc/timers.h
+++ Inc/timers.h
@@ -8,12 +8,21 @@
 #ifndef TIMERS_H_
 #define TIMERS_H_
 
+#include "pins.h"
+
 #include "stm32f30x_conf.h"
 
 typedef struct{
-	int16_t hours,minutes,seconds,miliseconds;
+	int16_t hours,minutes,seconds,hndrseconds,miliseconds,flag;
 } timeS_t;
 
+
+
+extern timeS_t tid;
+extern volatile uint32_t elapsed_time;
+extern volatile uint32_t elapsed_time_enemy;
+extern volatile uint32_t elapsed_time_PlayerBullet;
+extern volatile uint32_t elapsed_time_Asteroid;
 
 
 void configTimer();
@@ -21,8 +30,12 @@ void TIM1_BRK_TIM15_IRQHandler(void);
 void prntClk();
 int32_t returnSec();
 int32_t returnMilisec();
+int32_t returnHNDR();
+//void clockCounter(timeS_t* tid);
+void tidCalc (timeS_t* tid);
 
-timeS_t tid;
+
+
 
 
 #endif /* TIMERS_H_ */
