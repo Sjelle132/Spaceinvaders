fbdca4f8261060dcd2db81f717048da18274e97e
liou0006 <135958682+liou0006@users.noreply.github.com>
2023-06-20 10:31:25 +0200

    h files for fillips documents

--- Inc/asteroids.h
+++ Inc/asteroids.h
@@ -2,18 +2,23 @@
 
 #ifndef ASTEROIDS_H_
 #define ASTEROIDS_H_
+
 #include "stm32f30x_conf.h"
+#include "bullet.h"
 
 typedef struct {
+
     int32_t posX, posY;
     int8_t life;
-} asteroid_t;
 
+} asteroids_t;
 
-void initAsteroid(asteroid_t asteroid[]);
-void drawAsteroid(asteroid_t asteroid[]);
-void removeAsteroid(asteroid_t asteroid[]);
+void initAsteroids(asteroids_t asteroid[]);
+void drawAsteroid(asteroids_t asteroid[]);
+void removeAsteroid(asteroids_t asteroid[]);
 
+void applyGravity(asteroids_t asteroid[], bullet_t* bullet);
+void collisionDetectionA(asteroids_t asteroid[], bullet_t* bullet);
 
 
 #endif /* ASTEROIDS_H_ */
--- Inc/bullet.h
+++ Inc/bullet.h
@@ -11,26 +11,36 @@
 
 #include "stm32f30x_conf.h"
 #include "spaceship.h"
-#include "asteroids.h"
+#include "enemies.h"
+#include "ansi.h"
+#include "lcd.h"
+#include "uart.h"
+#include "boss.h"
 
 typedef struct {
+
 	int16_t posX;
 	int16_t posY;
 	int16_t velX;
 	int16_t velY;
 	int8_t true;
+	char score;
+
 } bullet_t;
 
+extern int8_t numAsteroids;
+
 void initBullet(bullet_t* bullet, spaceship_t* spaceship);
-uint32_t calculateDistance(int32_t x1, int32_t y1, int32_t x2, int32_t y2);
 void createBullet(bullet_t* bullet);
 void updateBullet(bullet_t* bullet);
-//void updateBullet(bullet_t* bullet,asteroid_t* asteroid, int numAsteroid);
 void removeBullet(bullet_t* bullet);
 void spawnBullet(bullet_t* bullet);
-void collisionDetectionA(asteroid_t asteroid[], bullet_t* bullet);
-void applyGravity(asteroid_t asteroid[],bullet_t* bullet, int8_t numAsteroids);
+//void drawAllBullets(bullet_t* bullet,spaceship_t* spaceship);
+//void updateBullet(bullet_t* bullet, asteroid_t* asteroid, int numAsteroids);
 
 
+void interactionsPlayerBulletHitEnemy(enemies_t enemies[], bullet_t* bullet );
+void interactionsPlayerBulletHitBoss(boss_t boss[], bullet_t* bullet );
 
+uint32_t calculateDistance(int32_t x1, int32_t y1, int32_t x2, int32_t y2);
 #endif /* BULLET_H_ */
